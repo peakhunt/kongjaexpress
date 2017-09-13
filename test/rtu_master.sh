b852501fcb6fe549fa205fcf87d6543e531acd80
Hawk Kim <hawk_kim74@hotmail.com>
2017-09-13 22:40:24 +0900

    changed test script timeout. updated todo

--- test/rtu_master.sh
+++ test/rtu_master.sh
@@ -2,7 +2,7 @@
 
 SLAVE_ADDR=1
 INTERVAL=50
-TIMEOUT=10.0
+TIMEOUT=3.0
 BAUD=9600
 
 ./modpoll -m rtu -a $SLAVE_ADDR -r 100 -c 64 -t 4 -l $INTERVAL -o $TIMEOUT -b $BAUD -d 8 -s 1 -p none /dev/ttyUSB1
